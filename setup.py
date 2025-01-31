5a218a080fa1b70e576fd7b2d4eef9eae63ed946
rezayas <reza.yaesoubi@yale.edu>
2025-01-31 12:33:16 -0500

    q function working for both

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name='deampy',
-    version='1.4.2',
+    version='1.4.3',
     install_requires=['numpy', 'matplotlib', 'scipy', 'statsmodels', 'scikit-learn'],
     packages=['deampy', 'deampy.optimization', 'deampy.plots', 'deampy.support'],
     url='https://github.com/modeling-health-care-decisions/deampy',
