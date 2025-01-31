6680980ff0e727b9afc4c1f0537e5320a1525f04
rezayas <reza.yaesoubi@yale.edu>
2025-01-31 12:31:43 -0500

    q function working for both

--- tests/regression/test_qfunction.py
+++ tests/regression/test_qfunction.py
@@ -82,48 +82,8 @@ for i in range(N):
 
 print('\nRegression with both: ')
 print('True Coeffs:', coeffs, 'vs.\nEsti Coeffs: ', q.get_coeffs())
+print('True y([2, -1], [0]) = ', np.dot([1, 2, -1, 4, -2, 1, 0, 0, 0, 0, 0, 0], coeffs), 'vs.\n'
+      'Esti y([2, -1], [0]) = ', q.f(x=[2, -1], binaries=[0]))
+print('True y([2, -1], [1]) = ', np.dot([1, 2, -1, 4, -2, 1, 1, 2, -1, 4, -2, 1], coeffs), 'vs.\n'
+      'Esti y([2, -1], [1]) = ', q.f(x=[2, -1], binaries=[1]))
 
-
-
-
-
-
-#
-# # testing regression model
-# # y = 2 + 5I + x1^2 + 4x1x2 - 8x2^2
-# # where I is a binary variable that takes 0 or 1 values
-# # and x1 and x2 are continuous variables
-# coeffs = [2, 5, 1, 4, -8] # coefficients of this regression model
-#
-# # generate X
-# Is = []
-# x1s = []
-# x2s = []
-# X = [] # for the regression
-# for n in range(N):
-#
-#     # get a random 0 or 1 for I
-#     I = np.random.randint(low=0, high=1)
-#     # get 2 random numbers over [-1, 1] for x1 and x2
-#     x = 2*np.random.sample(2)-1
-#
-#     # store observations on I, x1, and x2
-#     Is.append(I)
-#     x1s.append(x[0])
-#     x2s.append(x[1])
-#
-#     # store the row for the regression
-#     X.append([1, I, x[0]**2, x[0]*x[1], x[1]**2])
-#
-# # find y's
-# y = np.dot(X, np.array(coeffs)) + np.random.normal(0, sigma, N)
-#
-# # fit a polynomial regression
-# q = PolynomialQFunction(degree=2, l2_penalty=l2_reg)
-# for i in range(N):
-#     q.update(y=y[i],
-#              values_of_continuous_features=[x1s[i], x2s[i]],
-#              values_of_indicator_features=[Is[i]])
-#
-# print('Regression: ')
-# print('Coeffs: ', q.get_coeffs(), 'vs.', coeffs)
