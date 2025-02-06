e5eaa94b7f2721b39ce8563429af3f318d474dc2
rezayas <reza.yaesoubi@gmail.com>
2025-02-05 22:21:19 -0500

    learning_exploring_rules.py

--- deampy/optimization/learning_exploring_rules.py
+++ deampy/optimization/learning_exploring_rules.py
@@ -1,4 +1,4 @@
-from math import pow
+from math import pow, exp
 
 import matplotlib.pyplot as plt
 
@@ -22,28 +22,52 @@ class _LearningRule:
 
 class EpsilonGreedy(_ExplorationRule):
     # For selecting the greedy action with probability 1-epsilon.
-    # epsilon_n = 1/n^beta, beta over (0.5, 1], n > 0
+    # for pow decay formula: epsilon_n = min + (max-min)/n^beta, beta over (0.5, 1], n > 0
+    # for exp decay formula: epsilon_n = min + (max-min) * exp(-beta * n), beta > 0, n > 0
 
-    def __init__(self, beta):
+    def __init__(self, formula, beta, min=0, max=1):
+        """
+        :param formula: (str) 'pow' or 'exp'
+        :param beta: (float) the decay rate
+        :param min: (float) the minimum epsilon
+        :param max: (float) the maximum epsilon
+        """
+        self._formula = formula
         self._beta = beta
+        self._min = min
+        self._max = max
 
     def __str__(self):
-        return 'Beta{}'.format(self._beta)
+        return '{}-beta{}-max{}-min{}'.format(self._formula, self._beta, self._max, self._min)
 
     def get_epsilon(self, itr):
-        return pow(itr, -self._beta)
+
+        if self._formula == 'pow':
+            return self._min + (self._max - self._min) * pow(itr, -self._beta)
+        elif self._formula == 'exp':
+            return self._min + (self._max - self._min) * exp(-self._beta * itr)
+        else:
+            raise ValueError('Invalid formula. Choose between "power" and "exponential".')
 
     @staticmethod
-    def plot(betas, n_itrs):
+    def plot(formula, betas, maxes, mins, n_itrs):
+
         x = range(1, n_itrs + 1)
-        for beta in betas:
-            y = [EpsilonGreedy(beta).get_epsilon(i) for i in x]
-            plt.plot(x, y, label='Beta={}'.format(beta))
-        plt.xlabel('Iteration')
-        plt.ylabel('Epsilon')
-        plt.title('Epsilon-Greedy Exploration Rule')
-        plt.legend()
-        plt.show()
+
+        fig, ax = plt.subplots()
+
+        for max in maxes:
+            for min in mins:
+                for beta in betas:
+                    rule = EpsilonGreedy(formula=formula, beta=beta, max=max, min=min)
+                    y = [rule.get_epsilon(i) for i in x]
+                    ax.plot(x, y, label=str(rule))
+        ax.axhline(y=0, color='black', linestyle='--')
+        ax.set_xlabel('Iteration')
+        ax.set_ylabel('Epsilon')
+        ax.set_title('Epsilon-Greedy Exploration Rule')
+        ax.legend()
+        fig.show()
 
 
 class Harmonic(_LearningRule):
@@ -61,17 +85,26 @@ class Harmonic(_LearningRule):
 
     @staticmethod
     def plot(bs, n_itrs):
+
         x = range(1, n_itrs + 1)
+
+        fig, ax = plt.subplots()
         for b in bs:
-            y = [Harmonic(b).get_forgetting_factor(i) for i in x]
-            plt.plot(x, y, label='b={}'.format(b))
-        plt.plot(x, y)
-        plt.xlabel('Iteration')
-        plt.ylabel('Forgetting Factor')
-        plt.title('Harmonic Learning Rule')
-        plt.show()
+            rule = Harmonic(b)
+            y = [rule.get_forgetting_factor(i) for i in x]
+            ax.plot(x, y, label=str(rule))
+        ax.plot(x, y)
+        ax.axhline(y=1, color='black', linestyle='--')
+        ax.set_xlabel('Iteration')
+        ax.set_ylabel('Forgetting Factor')
+        ax.set_title('Harmonic Learning Rule')
+        ax.legend()
+        fig.show()
 
 
 if __name__ == '__main__':
-    EpsilonGreedy.plot(betas=[0.5, 0.75, 1], n_itrs=1000)
-    Harmonic.plot(bs=[1, 10, 20], n_itrs=1000)
+
+    EpsilonGreedy.plot(formula='exp', maxes=[1], mins=[0.05], betas=[0.01, 0.02, .03], n_itrs=1000)
+    EpsilonGreedy.plot(formula='pow', maxes=[1], mins=[0.05], betas=[0.5, 0.7, 0.9], n_itrs=1000)
+
+    # Harmonic.plot(bs=[1, 10, 20], n_itrs=1000)
