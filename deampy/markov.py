a7027cc087b7683502a348c38fbcfe796f52036d
rezayas <reza.yaesoubi@gmail.com>
2025-04-07 16:54:08 -0400

    continuous time cohort markov added but not yet debugged

--- deampy/markov.py
+++ deampy/markov.py
@@ -6,6 +6,125 @@ from numpy.random import RandomState
 from deampy.random_variates import Empirical, Exponential, Multinomial
 
 
+def continuous_to_discrete(trans_rate_matrix, delta_t):
+    """
+    :param trans_rate_matrix: (list of lists) transition rate matrix (assumes None or 0 for diagonal elements)
+    :param delta_t: cycle length
+    :return: transition probability matrix (list of lists)
+             and the upper bound for the probability of two transitions within delta_t (float)
+        converting [p_ij] to [lambda_ij] where
+            mu_i = sum of rates out of state i
+            p_ij = exp(-mu_i*delta_t),      if i = j,
+            p_ij = (1-exp(-mu_i*delta_t))*lambda_ij/mu_i,      if i != j.
+
+    """
+
+    # list of rates out of each row
+    rates_out = []
+    for i, row in enumerate(trans_rate_matrix):
+        rates_out.append(out_rate(row, i))
+
+    prob_matrix = []
+    for i in range(len(trans_rate_matrix)):
+        prob_row = []   # list of probabilities
+        # calculate probabilities
+        for j in range(len(trans_rate_matrix[i])):
+            prob = 0
+            if i == j:
+                prob = np.exp(-rates_out[i] * delta_t)
+            else:
+                if rates_out[i] > 0:
+                    prob = (1 - np.exp(-rates_out[i] * delta_t)) * trans_rate_matrix[i][j] / rates_out[i]
+            # append this probability
+            prob_row.append(prob)
+
+        # append this row of probabilities
+        prob_matrix.append(prob_row)
+
+    # probability that transition occurs within delta_t for each state
+    probs_out = []
+    for rate in rates_out:
+        probs_out.append(1-np.exp(-delta_t*rate))
+
+    # calculate the probability of two transitions within delta_t for each state
+    prob_out_out = []
+    for i in range(len(trans_rate_matrix)):
+
+        # probability of leaving state i withing delta_t
+        prob_out_i = probs_out[i]
+
+        # probability of leaving the new state after i withing delta_t
+        prob_out_again = 0
+
+        for j in range(len(trans_rate_matrix[i])):
+            if not i == j:
+
+                # probability of transition from i to j
+                prob_i_j = 0
+                if rates_out[i]>0:
+                    prob_i_j = trans_rate_matrix[i][j] / rates_out[i]
+                # probability of transition from i to j and then out of j within delta_t
+                prob_i_j_out = prob_i_j * probs_out[j]
+                # update the probability of transition out of i and again leaving the new state
+                prob_out_again += prob_i_j_out
+
+        # store the probability of leaving state i to a new state and leaving the new state withing delta_t
+        prob_out_out.append(prob_out_i*prob_out_again)
+
+    # return the probability matrix and the upper bound for the probability of two transitions with delta_t
+    return prob_matrix, max(prob_out_out)
+
+
+def out_rate(rates, idx):
+    """
+    :param rates: list of rates leaving this state
+    :param inx: index of this state
+    :returns the rate of leaving this state (the sum of rates)
+    """
+
+    sum_rates = 0
+    for i, v in enumerate(rates):
+        if i != idx and v is not None:
+            sum_rates += v
+    return sum_rates
+
+
+def discrete_to_continuous(trans_prob_matrix, delta_t):
+    """
+    :param trans_prob_matrix: (list of lists) transition probability matrix
+    :param delta_t: cycle length
+    :return: (list of lists) transition rate matrix
+        Converting [p_ij] to [lambda_ij] where
+            lambda_ii = None, and
+            lambda_ij = -ln(p_ii) * p_ij / ((1-p_ii)*Delta_t)
+    """
+
+    assert type(trans_prob_matrix) == list, \
+        "prob_matrix is a matrix that should be represented as a list of lists: " \
+        "For example: [ [0.1, 0.9], [0.8, 0.2] ]."
+
+    rate_matrix = []
+    for i, row in enumerate(trans_prob_matrix):
+        rate_row = []   # list of rates
+        # calculate rates
+        for j in range(len(row)):
+            # rate is None for diagonal elements
+            if i == j:
+                rate = None
+            else:
+                # rate is zero if this is an absorbing state
+                if trans_prob_matrix[i][i] == 1:
+                    rate = 0
+                else:
+                    rate = -np.log(trans_prob_matrix[i][i]) * trans_prob_matrix[i][j] / ((1 - trans_prob_matrix[i][i]) * delta_t)
+            # append this rate
+            rate_row.append(rate)
+        # append this row of rates
+        rate_matrix.append(rate_row)
+
+    return rate_matrix
+
+
 class _Markov:
 
     def __init__(self, matrix, state_descriptions=None):
@@ -49,6 +168,7 @@ class _Markov:
             current_state_index = current_state.value
 
         return current_state_index
+
 
 class MarkovJumpProcess(_Markov):
 
@@ -180,25 +300,69 @@ class Gillespie(_Markov):
         return dt, i
 
 
-class CohortMarkovProcess:
+class _CohortMarkov:
 
-    def __init__(self, transition_prob_matrix, state_descriptions=None):
+    def __init__(self):
+
+        self._numInStates = [] # list of state sizes
+        self._numInStatesOverTime = [] # list of number of patients in each state over time
+        self._numToStatesOverTime = [] # list of number of transitions to each state over time
+        self._n_time_steps = 0
+
+    def get_state_size_over_time(self, state_index):
+        """
+        :return: list of state sizes
+        """
+
+        return self._numInStatesOverTime[state_index]
+
+    def get_transition_to_states_over_time(self):
+        """
+        :return: list of number of transitions to each state
+        """
+
+        return self._numToStatesOverTime
+
+    def get_sum_size_multiple_states(self, state_indices):
+        """
+        :param state_indices: list of indices of states
+        :return: sum of the sizes of the states in the list
+        """
+
+        sum_size = 0
+        for i in state_indices:
+            sum_size += self._numInStates[i]
+
+        return sum_size
+
+    def get_sum_size_multiple_states_over_time(self, state_indices):
+        """
+        :param state_indices: list of indices of states
+        :return: sum of the sizes of the states in the list
+        """
+
+        sum_size = np.zeros(self._n_time_steps+1)
+        for i in state_indices:
+            sum_size += np.array(self._numInStatesOverTime[i])
+
+        return sum_size
+
+
+class CohortMarkov(_CohortMarkov):
+
+    def __init__(self, transition_prob_matrix):
         """
         :param transition_prob_matrix: (list of lists) transition probability matrix
-        :param state_descriptions: (Enum) description of the states in the format of Enum
         """
 
         assert type(transition_prob_matrix) is list, \
             'Transition probability matrix should be a list'
 
-        self._transition_prob_matrix = transition_prob_matrix
-        self._state_descriptions = state_descriptions
+        _CohortMarkov.__init__(self)
 
+        self._transition_prob_matrix = transition_prob_matrix
         self._nonZeroProbs = [] # list of non-zero probabilities
         self._indicesNonZeroProbs = [] # list of indices of non-zero probabilities
-        self._numInStates = [] # list of state sizes
-        self._numInStatesOverTime = [] # list of number of patients in each state over time
-        self._numToStatesOverTime = [] # list of number of transitions to each state over time
 
         for i, probs in enumerate(transition_prob_matrix):
 
@@ -220,7 +384,7 @@ class CohortMarkovProcess:
             self._indicesNonZeroProbs.append(non_zero_probs_indices)
 
         self._n_states = len(self._transition_prob_matrix)
-        self._n_time_steps = 0
+
 
     def simulate(self, initial_condition, n_time_steps, rng=None):
 
@@ -266,159 +430,32 @@ class CohortMarkovProcess:
             self._numInStatesOverTime[i].append(self._numInStates[i])
 
 
-    def get_state_size_over_time(self, state_index):
-        """
-        :return: list of state sizes
-        """
+class ContinuousTimeCohortMarkov(_CohortMarkov):
 
-        return self._numInStatesOverTime[state_index]
-
-    def get_transition_to_states_over_time(self):
+    def __init__(self, transition_rate_matrix, delta_t):
         """
-        :return: list of number of transitions to each state
+        :param transition_rate_matrix: (list of lists) transition rate matrix
+        :param delta_t: (float) cycle length
         """
 
-        return self._numToStatesOverTime
+        assert isinstance(transition_rate_matrix, list), \
+            'transition_rate_matrix should be an array, {} was provided'.format(type(transition_rate_matrix))
 
-    def get_sum_size_multiple_states(self, state_indices):
+        _CohortMarkov.__init__(self)
+
+        self._markov = CohortMarkov(
+            transition_prob_matrix = continuous_to_discrete(
+                trans_rate_matrix=transition_rate_matrix,
+                delta_t= delta_t))
+
+    def simulate(self, initial_condition, n_time_steps, rng=None):
         """
-        :param state_indices: list of indices of states
-        :return: sum of the sizes of the states in the list
-        """
-
-        sum_size = 0
-        for i in state_indices:
-            sum_size += self._numInStates[i]
-
-        return sum_size
-
-    def get_sum_size_multiple_states_over_time(self, state_indices):
-        """
-        :param state_indices: list of indices of states
-        :return: sum of the sizes of the states in the list
+        :param initial_condition: (list) initial size of each state
+        :param n_time_steps: (int) number of time steps to simulate the cohort
+        :param rng: random number generator object
         """
 
-        sum_size = np.zeros(self._n_time_steps+1)
-        for i in state_indices:
-            sum_size += np.array(self._numInStatesOverTime[i])
-
-        return sum_size
-
-
-def continuous_to_discrete(trans_rate_matrix, delta_t):
-    """
-    :param trans_rate_matrix: (list of lists) transition rate matrix (assumes None or 0 for diagonal elements)
-    :param delta_t: cycle length
-    :return: transition probability matrix (list of lists)
-             and the upper bound for the probability of two transitions within delta_t (float)
-        converting [p_ij] to [lambda_ij] where
-            mu_i = sum of rates out of state i
-            p_ij = exp(-mu_i*delta_t),      if i = j,
-            p_ij = (1-exp(-mu_i*delta_t))*lambda_ij/mu_i,      if i != j.
-
-    """
-
-    # list of rates out of each row
-    rates_out = []
-    for i, row in enumerate(trans_rate_matrix):
-        rates_out.append(out_rate(row, i))
-
-    prob_matrix = []
-    for i in range(len(trans_rate_matrix)):
-        prob_row = []   # list of probabilities
-        # calculate probabilities
-        for j in range(len(trans_rate_matrix[i])):
-            prob = 0
-            if i == j:
-                prob = np.exp(-rates_out[i] * delta_t)
-            else:
-                if rates_out[i] > 0:
-                    prob = (1 - np.exp(-rates_out[i] * delta_t)) * trans_rate_matrix[i][j] / rates_out[i]
-            # append this probability
-            prob_row.append(prob)
-
-        # append this row of probabilities
-        prob_matrix.append(prob_row)
-
-    # probability that transition occurs within delta_t for each state
-    probs_out = []
-    for rate in rates_out:
-        probs_out.append(1-np.exp(-delta_t*rate))
-
-    # calculate the probability of two transitions within delta_t for each state
-    prob_out_out = []
-    for i in range(len(trans_rate_matrix)):
-
-        # probability of leaving state i withing delta_t
-        prob_out_i = probs_out[i]
-
-        # probability of leaving the new state after i withing delta_t
-        prob_out_again = 0
-
-        for j in range(len(trans_rate_matrix[i])):
-            if not i == j:
-
-                # probability of transition from i to j
-                prob_i_j = 0
-                if rates_out[i]>0:
-                    prob_i_j = trans_rate_matrix[i][j] / rates_out[i]
-                # probability of transition from i to j and then out of j within delta_t
-                prob_i_j_out = prob_i_j * probs_out[j]
-                # update the probability of transition out of i and again leaving the new state
-                prob_out_again += prob_i_j_out
-
-        # store the probability of leaving state i to a new state and leaving the new state withing delta_t
-        prob_out_out.append(prob_out_i*prob_out_again)
-
-    # return the probability matrix and the upper bound for the probability of two transitions with delta_t
-    return prob_matrix, max(prob_out_out)
-
-
-def out_rate(rates, idx):
-    """
-    :param rates: list of rates leaving this state
-    :param inx: index of this state
-    :returns the rate of leaving this state (the sum of rates)
-    """
-
-    sum_rates = 0
-    for i, v in enumerate(rates):
-        if i != idx and v is not None:
-            sum_rates += v
-    return sum_rates
-
-
-def discrete_to_continuous(trans_prob_matrix, delta_t):
-    """
-    :param trans_prob_matrix: (list of lists) transition probability matrix
-    :param delta_t: cycle length
-    :return: (list of lists) transition rate matrix
-        Converting [p_ij] to [lambda_ij] where
-            lambda_ii = None, and
-            lambda_ij = -ln(p_ii) * p_ij / ((1-p_ii)*Delta_t)
-    """
-
-    assert type(trans_prob_matrix) == list, \
-        "prob_matrix is a matrix that should be represented as a list of lists: " \
-        "For example: [ [0.1, 0.9], [0.8, 0.2] ]."
-
-    rate_matrix = []
-    for i, row in enumerate(trans_prob_matrix):
-        rate_row = []   # list of rates
-        # calculate rates
-        for j in range(len(row)):
-            # rate is None for diagonal elements
-            if i == j:
-                rate = None
-            else:
-                # rate is zero if this is an absorbing state
-                if trans_prob_matrix[i][i] == 1:
-                    rate = 0
-                else:
-                    rate = -np.log(trans_prob_matrix[i][i]) * trans_prob_matrix[i][j] / ((1 - trans_prob_matrix[i][i]) * delta_t)
-            # append this rate
-            rate_row.append(rate) 
-        # append this row of rates
-        rate_matrix.append(rate_row)
-
-    return rate_matrix
+        self._markov.simulate(
+            initial_condition=initial_condition,
+            n_time_steps=n_time_steps,
+            rng=rng)
